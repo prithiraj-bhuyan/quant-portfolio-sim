da6c734eca32770f1d4c04629c43ddb55577b7a9
Prithiraj <prithiraj111@gmail.com>
2026-01-31 12:38:30 -0500

    weights and portfolio value calculations

---
+++ portfolio_service.py
@@ -0,0 +1,29 @@
+from models import PortfolioItem
+from sqlmodel import Session, select
+from data_service import get_portfolio_data
+import numpy as np
+
+def get_simulation_inputs(session: Session):
+    items = session.exec(select(PortfolioItem)).all()
+    if not items:
+        return None
+    
+    ticker_list = [item.ticker for item in items]
+    quantities = np.array([item.quantity for item in items])
+
+    returns_df, curr_prices = get_portfolio_data(ticker_list)
+    curr_prices = np.array(curr_prices)
+
+    ticker_values = quantities * curr_prices
+    portfolio_total_val = np.sum(ticker_values)
+
+    weights = ticker_values/portfolio_total_val
+
+    return {
+        "returns_df": returns_df,
+        "weights": weights,
+        "current_prices": curr_prices,
+        "total_value": portfolio_total_val
+    }
+    
+
